81acd564b0b2729accbb56b2da6e5241ddcd8b42
K-nass <karimnase10@gmail.com>
2025-12-26 14:11:52 +0200

    fixed the header

--- menu/app/layout.tsx
+++ menu/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import "./globals.css";
 import Header from "./Header";
 export default function RootLayout({
@@ -9,7 +10,9 @@ export default function RootLayout({
     <html lang="en">
       <body className="bg-yellow-200">
         <div className="container mx-auto px-2 py-4">
-          <Header />
+          <Suspense fallback={null}>
+            <Header />
+          </Suspense>
           {children}
         </div>
       </body>
